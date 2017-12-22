2891bc5ceeefb94a90d56df6940609c3557629a2
a4anna <shilpa.a.george@gmail.com>
2017-12-21 20:23:02 -0500

    add in screensaver setup

--- gabriel-client-style-python/client.py
+++ gabriel-client-style-python/client.py
@@ -18,6 +18,9 @@ import base64
 import os
 import protocol
 from socketLib import ClientCommand, ClientReply, SocketClientThread
+import errno
+import posix
+import random
 
 class GabrielSocketCommand(ClientCommand):
     STREAM=len(ClientCommand.ACTIONS)
@@ -35,6 +38,7 @@ class VideoStreamingThread(SocketClientThread):
         self.handlers[GabrielSocketCommand.STREAM] = self._handle_STREAM
         self.is_streaming=False
         self.style_array = os.listdir('./style-image')
+        random.shuffle(self.style_array)
         #self.style_array = self.style_array[1:]
         self.length = len(self.style_array)
         self.SEC = 30
@@ -70,7 +74,7 @@ class VideoStreamingThread(SocketClientThread):
             tokenm.getToken()
             ret, frame = video_capture.read()
             capture_time = time.time()
-            print("Capture Time: {} {}".format(capture_time,video_capture.get(cv2.CAP_PROP_FPS)))
+            # print("Capture Time: {} {}".format(capture_time,video_capture.get(cv2.CAP_PROP_FPS)))
             frame = cv2.flip(frame,1)
             frame = cv2.resize(frame,(640,360))
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -80,7 +84,7 @@ class VideoStreamingThread(SocketClientThread):
             #header={protocol.Protocol_client.JSON_KEY_STYLE : "udnie"}
             header_json=json.dumps(header)
             send_time = time.time()
-            print("Send Time: {}".format(send_time))
+            # print("Send Time: {}".format(send_time))
             self._handle_SEND(ClientCommand(ClientCommand.SEND, header_json))
             self._handle_SEND(ClientCommand(ClientCommand.SEND, jpeg_frame.tostring()))
             id+=1
@@ -114,7 +118,7 @@ class ResultReceivingThread(SocketClientThread):
                         header, data = self._recv_gabriel_data()
                         self.reply_q.put(self._success_reply( (header, data) ))
                         recv_time = time.time()
-                        print("Recv Time: {}".format(recv_time))
+                        # print("Recv Time: {}".format(recv_time))
                         tokenm.putToken()
         
     def _recv_gabriel_data(self):
@@ -174,13 +178,20 @@ def run(sig_frame_available=None):
     sleep(0.1)
     video_streaming_thread.start()
 
+    if sig_frame_available is None:
+        rgbpipe_path = '/tmp/rgbpipe'
+        if not os.path.exists(rgbpipe_path):
+            os.mkfifo(rgbpipe_path)
+        rgbpipe = os.open(rgbpipe_path, os.O_WRONLY)
+    
     try:
         while True:
+            sys.stdout.flush()
             resp=result_reply_q.get()
             # connect and send also send reply to reply queue without any data attached
             if resp.type == ClientReply.SUCCESS and resp.data is not None:
                 tkn_time = time.time()
-                print("Tocken Time: {}".format(tkn_time))
+                # print("Tocken Time: {}".format(tkn_time))
                 (resp_header, resp_data) =resp.data
                 resp_header=json.loads(resp_header)
                 img=resp_data
@@ -190,29 +201,46 @@ def run(sig_frame_available=None):
                 if sig_frame_available == None:
                     #print 'resp:{}'.format(img[:100])
                     # when there is no qt window, just dump the image into stdout
+                    st = time.time()
                     rgb_frame = frame
-                    style_image = cv2.imread(os.path.join('./style-image', resp_header['style'] + '.jpg'), -1)
-                    style_image = cv2.cvtColor(style_image, cv2.COLOR_BGR2RGB)
-                    im_h, im_w, _ = rgb_frame.shape
+                    # style_image = cv2.imread(os.path.join('./style-image', resp_header['style'] + '.jpg'), -1)
+                    # style_image = cv2.cvtColor(style_image, cv2.COLOR_BGR2RGB)
+                    # im_h, im_w, _ = rgb_frame.shape
+                    style_image = style_name_to_image[resp_header['style']]
                     style_im_h, style_im_w, _ = style_image.shape
-                    style_im_aspect_ratio = style_im_h / float(style_im_w)
-                    resize_to_h = im_h * 0.3
-                    resize_to_w = resize_to_h / style_im_aspect_ratio
-                    style_image_resized = cv2.resize(style_image, (int(resize_to_w), int(resize_to_h)))
-                    rgb_frame[0:resize_to_h, 0:resize_to_w, :] = style_image_resized
-                    cv2.rectangle(rgb_frame, (0,0), (int(resize_to_w), int(resize_to_h)), (255,0,0), 4)
-                    sys.stderr.write(rgb_frame)
-                    sys.stderr.flush()
+                    # style_im_aspect_ratio = style_im_h / float(style_im_w)
+                    # resize_to_h = im_h * 0.3
+                    # resize_to_w = resize_to_h / style_im_aspect_ratio
+                    # style_image_resized = cv2.resize(style_image, (int(resize_to_w), int(resize_to_h)))
+                    rgb_frame[0:style_im_h, 0:style_im_w, :] = style_image
+                    cv2.rectangle(rgb_frame, (0,0), (int(style_im_w), int(style_im_h)), (255,0,0), 3)
+                    rgb_frame_enlarged = cv2.resize(rgb_frame, None, fx=2, fy=2)
+                    print('resize tooks: {}'.format(time.time()-st))
+                    st = time.time()
+                    os.write(rgbpipe, rgb_frame_enlarged.tostring())
+                    print('write to pipe tooks: {}'.format(time.time()-st))
+                    # sys.stderr.write(rgb_frame_enlarged)
                 else:
                     # display received image on the pyqt ui
                     #rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)    
                     #print("HEADER STYLE {}".format(resp_header['style']))                
                     sig_frame_available.emit(frame,resp_header['style'])
     except KeyboardInterrupt:
+        os.close(rgbpipe)
         video_streaming_thread.join()
         result_receiving_thread.join()
         with tokenm.has_token_cv:
             tokenm.has_token_cv.notifyAll()
 
+def _load_style_images(style_dir_path='style-image'):
+    style_name_to_image = {}
+    for image_name in os.listdir(style_dir_path):
+        im = cv2.imread(os.path.join(style_dir_path, image_name))
+        im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+        style_name_to_image[os.path.splitext(image_name)[0]] = im
+    return style_name_to_image
+
+
 if __name__ == '__main__':
+    style_name_to_image = _load_style_images('style-image')
     run()
---
+++ gabriel-client-style-python/run_pipe_stream_for_screensaver.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+stream_pipe="/tmp/stylepipe"
+rm $stream_pipe
+while true; do
+    if [[ -p $stream_pipe ]]; then
+	rm /tmp/rgbpipe;
+	mkfifo /tmp/rgbpipe;
+	./client.py &
+	bgid=$!
+	cat /tmp/rgbpipe | ffmpeg -f rawvideo -pixel_format rgb24 -video_size 1280x720 -re -hwaccel vaapi -i - -f rawvideo -video_size 1280x720 -pix_fmt rgb24 - > $stream_pipe
+	kill -9 $bgid
+	rm -f  $stream_pipe
+	sleep 5
+    else
+	echo "streaming pipe not found. wait for it to be created..."
+	sleep 5
+    fi
+done
