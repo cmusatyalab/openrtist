0109f05283ca74e92145c7393c4a75c28ec16dbc
Thomas Eiszler <teiszler@cs.cmu.edu>
2019-03-12 15:03:19 -0400

    Allow toggling between cameras during style transfer. Updated icons for screenshot, recording, camera orientation.

--- gabriel-client-openrtist-android/app/src/main/java/edu/cmu/cs/gabriel/network/VideoStreamingThread.java
+++ gabriel-client-openrtist-android/app/src/main/java/edu/cmu/cs/gabriel/network/VideoStreamingThread.java
@@ -251,7 +251,7 @@ public class VideoStreamingThread extends Thread {
                 ByteArrayOutputStream tmpBuffer = new ByteArrayOutputStream();
                 // chooses quality 67 and it roughly matches quality 5 in avconv
                 image.compressToJpeg(new Rect(0, 0, image.getWidth(), image.getHeight()), 67, tmpBuffer);
-                if(Const.FRONT_CAMERA_ENABLED) {
+                if(Const.USING_FRONT_CAMERA) {
                     byte[] newFrame = tmpBuffer.toByteArray();
                     Bitmap bitmap = BitmapFactory.decodeByteArray(newFrame, 0, newFrame.length);
                     ByteArrayOutputStream rotatedStream = new ByteArrayOutputStream();
