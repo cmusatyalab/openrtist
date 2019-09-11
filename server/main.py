95fef3d5fbc5897a15cf591215a4a5c5ff12a6d9
Roger Iyengar <ri@rogeriyengar.com>
2019-09-11 02:43:51 -0400

    Git submodules are garbage

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 from gabriel_server.local_engine import runner
 from openrtist_engine import OpenrtistEngine
+from openrtist_engine import ENGINE_NAME
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -8,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 def main():
     def engine_setup():
         return OpenrtistEngine(use_gpu=True)
-    runner.run(engine_setup)
+    runner.run(engine_setup, ENGINE_NAME)
 
 
 if __name__ == '__main__':
