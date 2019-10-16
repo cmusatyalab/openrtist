4d7b479fc8ace205f56755dfefb4872e6cb69447
Ubuntu <ubuntu@cloudlet030.maas>
2019-10-16 22:02:36 +0000

    Fix bugs

--- server/main.py
+++ server/main.py
@@ -1,13 +1,11 @@
 from gabriel_server.local_engine import runner
 from openrtist_engine import OpenrtistEngine
-from openrtist_engine import ENGINE_NAME
 import logging
 import cv2
 
 logging.basicConfig(level=logging.INFO)
 
 USE_GPU = True
-ENGINE_NAME = 'openrtist'
 DEFAULT_STYLE = 'the_scream'
 COMPRESSION_PARAMS = [cv2.IMWRITE_JPEG_QUALITY, 67]
 PORT = 9098
@@ -17,9 +15,10 @@ INPUT_QUEUE_MAXSIZE = 2
 
 def main():
     def engine_setup():
-        return OpenrtistEngine(USE_GPU, ENGINE_NAME, DEFAULT_STYLE,
+        return OpenrtistEngine(USE_GPU, DEFAULT_STYLE,
                                COMPRESSION_PARAMS)
-    runner.run(engine_setup, ENGINE_NAME, INPUT_QUEUE_MAXSIZE, PORT, NUM_TOKENS)
+    runner.run(engine_setup, OpenrtistEngine.ENGINE_NAME,
+               INPUT_QUEUE_MAXSIZE, PORT, NUM_TOKENS)
 
 
 if __name__ == '__main__':
