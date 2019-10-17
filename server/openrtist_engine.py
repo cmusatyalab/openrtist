bb37962e3f4c3a17a32efd8f783d3dbe6328b638
Roger Iyengar <ri@rogeriyengar.com>
2019-10-17 00:29:54 -0400

    base class for engine

--- server/openrtist_engine.py
+++ server/openrtist_engine.py
@@ -2,66 +2,54 @@ import os
 import cv2
 import numpy as np
 import logging
-import torch
-from torch.autograd import Variable
-from torch.optim import Adam
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision import transforms
-from PIL import Image
-from transformer_net import TransformerNet
 from gabriel_server import cognitive_engine
 from gabriel_protocol import gabriel_pb2
 from google.protobuf.any_pb2 import Any
 from openrtist_protocol import openrtist_pb2
+from abc import abstractmethod
 
 logger = logging.getLogger(__name__)
 
 
-# TODO: support openvino
 class OpenrtistEngine(cognitive_engine.Engine):
     ENGINE_NAME = 'openrtist'
-    
-    def __init__(self, use_gpu, default_style, compression_params):
+
+    def __init__(self, default_style, compression_params):
+        self.style = default_style
+        self.compression_params = compression_params
+
         self.dir_path = os.getcwd()
         self.model = self.dir_path+'/../models/the_scream.model'
         self.path = self.dir_path+'/../models/'
-
-        self.style_model = TransformerNet()
-        self.style_model.load_state_dict(torch.load(self.model))
-
-        self.use_gpu = use_gpu
-        if (use_gpu):
-            self.style_model.cuda()
-
-        self.content_transform = transforms.Compose([transforms.ToTensor()])
-        self.style = default_style
 
         wtr_mrk4 = cv2.imread('../wtrMrk.png',-1) # The waterMark is of dimension 30x120
         self.mrk,_,_,mrk_alpha = cv2.split(wtr_mrk4) # The RGB channels are equivalent
         self.alpha = mrk_alpha.astype(float)/255
 
-        self.compression_params = compression_params
-
         # TODO support server display
 
         logger.info('FINISHED INITIALISATION')
 
+    @abstractmethod
+    def change_style(self, new_style):
+        pass
+
+    @abstractmethod
+    def run_model(self, img):
+        pass
+
     def handle(self, from_client):
+        if (from_client.payload_type != gabriel_pb2.PayloadType.IMAGE):
+            return cognitive_engine.wrong_input_format_error(
+                from_client.frame_id)
+
         engine_fields = cognitive_engine.unpack_engine_fields(
             openrtist_pb2.EngineFields, from_client)
 
         if engine_fields.style != self.style:
-            self.model = self.path + engine_fields.style + ".model"
-            self.style_model.load_state_dict(torch.load(self.model))
-            if (self.use_gpu):
-                self.style_model.cuda()
+            self.change_style(engine_fields.style)
             self.style = engine_fields.style
             logger.info('New Style: %s', self.style)
-
-        if (from_client.payload_type != gabriel_pb2.PayloadType.IMAGE):
-            return cognitive_engine.wrong_input_format_error(
-                from_client.frame_id)
 
         image = self._process_image(from_client.payload)
         image = self._apply_watermark(image)
@@ -86,17 +74,8 @@ class OpenrtistEngine(cognitive_engine.Engine):
         np_data=np.fromstring(image, dtype=np.uint8)
         img=cv2.imdecode(np_data,cv2.IMREAD_COLOR)
         img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-        content_image = self.content_transform(img)
-        if (self.use_gpu):
-            content_image = content_image.cuda()
-        content_image = content_image.unsqueeze(0)
-        content_image = Variable(content_image, volatile=True)
 
-        output = self.style_model(content_image)
-        img_out = output.data[0].clamp(0, 255).cpu().numpy()
-        img_out = img_out.transpose(1, 2, 0)
-
-        return img_out
+        return self.run_model(img)
 
     def _apply_watermark(self, image):
         img_mrk = image[-30:,-120:] # The waterMark is of dimension 30x120
