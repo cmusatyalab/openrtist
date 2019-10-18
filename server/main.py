7c1ff25d0dcd9a7f8708d8bd9c6399c830f67a7b
a4anna <shilpag@cloudlet025.maas>
2019-10-18 12:08:55 +0000

    Modified to take token and engine as argument

--- server/main.py
+++ server/main.py
@@ -1,26 +1,41 @@
 #!/usr/bin/env python3
 
 from gabriel_server.local_engine import runner
-from torch_engine import TorchEngine
-# from openvino_engine import OpenvinoEngine
 import timing_engine
 import logging
 import cv2
+import argparse
 
 USE_GPU = True
 DEFAULT_STYLE = 'the_scream'
 COMPRESSION_PARAMS = [cv2.IMWRITE_JPEG_QUALITY, 67]
 PORT = 9098
 NUM_TOKENS = 2
-INPUT_QUEUE_MAXSIZE = 2
-ENGINE = TorchEngine
-# ENGINE = OpenvinoEngine
+INPUT_QUEUE_MAXSIZE = 60
+ENGINE = None
 TIMING = True
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--tokens", type=int, default=2, help="number of tokens")
+    parser.add_argument("-o", "--openvino", type=int, default=0, help="set it to 1 to use OpenVINO")
+    args = parser.parse_args()
+
+    NUM_TOKENS = args.tokens
+    print(args)
+
+    if args.openvino:
+        from openvino_engine import OpenvinoEngine
+        ENGINE = OpenvinoEngine
+    else:
+        from torch_engine import TorchEngine
+        ENGINE = TorchEngine
+
+    print(ENGINE)
+
     engine = (timing_engine.factory(ENGINE)
               if TIMING else ENGINE)
 
