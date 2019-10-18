c1aeeeea7bcf798df7a609f7b4444cf3d17da331
Roger Iyengar <ri@rogeriyengar.com>
2019-10-18 01:19:23 -0400

    Update torch_engine.py

--- server/torch_engine.py
+++ server/torch_engine.py
@@ -1,3 +1,34 @@
+# OpenRTiST
+#   - Real-time Style Transfer
+#
+#   Author: Zhuo Chen <zhuoc@cs.cmu.edu>
+#           Shilpa George <shilpag@andrew.cmu.edu>
+#           Thomas Eiszler <teiszler@andrew.cmu.edu>
+#           Padmanabhan Pillai <padmanabhan.s.pillai@intel.com>
+#           Roger Iyengar <iyengar@cmu.edu>
+#
+#   Copyright (C) 2011-2019 Carnegie Mellon University
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#
+# Portions of this code borrow from sample code distributed as part of 
+# Intel OpenVino, which is also distributed under the Apache License.
+# 
+# Portions of this code were modified from sampled code distributed as part of
+# the fast_neural_style example that is part of the pytorch repository and is
+# distributed under the BSD 3-Clause License.
+# https://github.com/pytorch/examples/blob/master/LICENSE
+
 from openrtist_engine import OpenrtistEngine
 from torch.autograd import Variable
 from transformer_net import TransformerNet
