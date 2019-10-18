7b1be7e1b96833ef6bcbad6e8782feb7ab9cc36f
Roger Iyengar <ri@rogeriyengar.com>
2019-10-18 00:13:12 -0400

    Added timing

--- server/main.py
+++ server/main.py
@@ -1,10 +1,9 @@
 from gabriel_server.local_engine import runner
-# from torch_engine import TorchEngine
+from torch_engine import TorchEngine
 from openvino_engine import OpenvinoEngine
+import timing_engine
 import logging
 import cv2
-
-logging.basicConfig(level=logging.INFO)
 
 USE_GPU = True
 DEFAULT_STYLE = 'the_scream'
@@ -12,12 +11,20 @@ COMPRESSION_PARAMS = [cv2.IMWRITE_JPEG_QUALITY, 67]
 PORT = 9098
 NUM_TOKENS = 2
 INPUT_QUEUE_MAXSIZE = 2
+ENGINE = TorchEngine
+# ENGINE = OpenvinoEngine
+TIMING = True
+
+logging.basicConfig(level=logging.INFO)
 
 
 def main():
+    engine = (timing_engine.factory(ENGINE)
+              if TIMING else ENGINE)
+
     def engine_setup():
-        return OpenvinoEngine(USE_GPU, DEFAULT_STYLE, COMPRESSION_PARAMS)
-    runner.run(engine_setup, OpenvinoEngine.ENGINE_NAME, INPUT_QUEUE_MAXSIZE,
+        return engine(USE_GPU, DEFAULT_STYLE, COMPRESSION_PARAMS)
+    runner.run(engine_setup, engine.ENGINE_NAME, INPUT_QUEUE_MAXSIZE,
                PORT, NUM_TOKENS)
 
 
