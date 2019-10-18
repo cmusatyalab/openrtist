308d92a50611c5ea2f9a290606a519c4a19f27fa
Haithem Turki <hturki@cs.cmu.edu>
2019-10-18 12:55:31 -0400

    initial commit

---
+++ model-app/app.py
@@ -0,0 +1,44 @@
+from PIL import Image
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from .train_style import train, get_args
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            train(Image.open(file.stream), get_args(['--dataset', app.config['DATASET']]))
+            #file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new Style</title>
+    <h1>Upload new Style</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
