feebd114a3b7c3236927f8d2b32840ba926d00f4
Haithem Turki <hturki@cs.cmu.edu>
2019-10-23 17:46:23 -0400

    added openvino support

--- model-app/config.py
+++ model-app/config.py
@@ -1,8 +1,8 @@
-DATASET="/home/ubuntu/coco-data/"
+DATASET="/home/ubuntu/coco-data-small/"
 UPLOAD_FOLDER="/home/ubuntu/styles"
 DOWNLOAD_FOLDER="/home/ubuntu/models"
+CONVERT_TO_OPEN_VINO=True
 
 SECRET_KEY = 'Eg_NyQaCC5q5Rqnw0iaFlxKlsFT4YdwAym2D1TCTt18'
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-
