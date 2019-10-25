98587cd31c207315c44172893de9974a7395379b
Junjue Wang <junjuew@cs.cmu.edu>
2019-10-25 15:24:43 -0400

    [scripts] add in copyright header

--- scripts/freeze_model.py
+++ scripts/freeze_model.py
@@ -1,3 +1,27 @@
+#!/usr/bin/env python
+#
+# OpenRTiST
+#   - Real-time Style Transfer
+#
+#   Author: Zhuo Chen <zhuoc@cs.cmu.edu>
+#           Shilpa George <shilpag@andrew.cmu.edu>
+#           Thomas Eiszler <teiszler@andrew.cmu.edu>
+#           Padmanabhan Pillai <padmanabhan.s.pillai@intel.com>
+#           Junjue Wang <junjuew.cs.cmu.edu>
+#
+#   Copyright (C) 2011-2019 Carnegie Mellon University
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 """Script to freeze and quantize a model for inference on mobile platforms 
 and other languages. Pytorch 1.3 is required.
 
