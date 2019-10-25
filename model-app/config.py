92412a7c024bf5e807ea2b2818521354aa1705d6
hturki <turki.haithem@gmail.com>
2019-10-25 14:53:23 -0400

    Update config.py

--- model-app/config.py
+++ model-app/config.py
@@ -3,6 +3,6 @@ UPLOAD_FOLDER="/home/ubuntu/styles"
 DOWNLOAD_FOLDER="/home/ubuntu/models"
 CONVERT_TO_OPEN_VINO=True
 
-SECRET_KEY = 'Eg_NyQaCC5q5Rqnw0iaFlxKlsFT4YdwAym2D1TCTt18'
+SECRET_KEY = 'change_this_in_production'
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
