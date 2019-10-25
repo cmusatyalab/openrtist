f4faaa6a099c0e077bfb79fc59ecfe358edbc5b9
Junjue Wang <junjuew@cs.cmu.edu>
2019-10-25 14:46:37 -0400

    [transformer_net] removed unused conditional program path for model tracing and freezing.

--- server/transformer_net.py
+++ server/transformer_net.py
@@ -113,20 +113,17 @@ class UpsampleConvLayer(torch.nn.Module):
     ref: http://distill.pub/2016/deconv-checkerboard/
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample):
         super(UpsampleConvLayer, self).__init__()
         self.upsample = upsample
-        if upsample:
-            self.upsample_layer = torch.nn.Upsample(scale_factor=upsample)
-            #self.upsample_layer = torch.nn.UpsamplingNearest2d(scale_factor=upsample)
+        self.upsample_layer = torch.nn.Upsample(scale_factor=upsample)
         reflection_padding = kernel_size // 2
         self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
         self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
 
     def forward(self, x):
         x_in = x
-        if self.upsample:
-            x_in = self.upsample_layer(x_in)
+        x_in = self.upsample_layer(x_in)
         out = self.reflection_pad(x_in)
         out = self.conv2d(out)
         return out
