76ec35c1c420bb19d72d0eb44fd4eb5c565aaadc
Roger Iyengar <ri@rogeriyengar.com>
2019-10-28 16:26:51 -0400

    Update tag. add Proto

--- gabriel-client-openrtist-android/app/src/mobileEdgeX/java/edu/cmu/cs/gabriel/MexConst.java
+++ gabriel-client-openrtist-android/app/src/mobileEdgeX/java/edu/cmu/cs/gabriel/MexConst.java
@@ -7,7 +7,7 @@ public class MexConst {
     public static String MEX_APP = "openrtist";
     public static String MEX_DEV = "CMU";
     public static String MEX_CARRIER = "CMU";
-    public static String MEX_TAG = "sepia";
+    public static String MEX_TAG = "pb";
     public static String MEX_DME_URL = "us-mexdemo.dme.mobiledgex.net";
 
     public static void loadPref(Context c, String key, Object value) {
