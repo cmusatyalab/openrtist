5f562edebf611ca2daf727760c94c8c34ddeb554
Padmanabhan Pillai <padmanabhan.s.pillai@intel.com>
2019-10-29 00:59:13 -0400

    python client fix

--- gabriel-client-openrtist-python/adapter.py
+++ gabriel-client-openrtist-python/adapter.py
@@ -18,6 +18,7 @@ from openrtist_protocol import openrtist_pb2
 import cv2
 import config
 import numpy as np
+import random
 
 
 class Adapter:
@@ -32,12 +33,17 @@ class Adapter:
     def set_style(self, style):
         self._style = style
 
-    def __init__(self, preprocess, consume_frame_style, video_capture):
+    def get_styles(self):
+        return self.available_styles
+
+    def __init__(self, preprocess, consume_frame_style, video_capture, start_style=config.START_STYLE_STRING):
         '''
         consume_frame_style takes one frame parameter and one style parameter
         '''
 
-        self._style = config.START_STYLE_STRING
+        self._style = start_style
+        self.available_styles = []
+        self.style_image = None
 
         def produce_engine_fields():
             engine_fields = openrtist_pb2.EngineFields()
@@ -47,8 +53,22 @@ class Adapter:
         def consume_frame(frame, packed_engine_fields):
             engine_fields = openrtist_pb2.EngineFields()
             packed_engine_fields.Unpack(engine_fields)
+            if self._style=='?':
+                self._style = engine_fields.style
+            if len(engine_fields.style_list) > 0:
+                self.available_styles = list(engine_fields.style_list.keys())
+                random.shuffle(self.available_styles)
+                #print (self.available_styles)
+            if engine_fields.HasField('style_image'):
+                if len(engine_fields.style_image.value)==0:
+                    self.style_image=None
+                    #print("got empty style image")
+                else:
+                    self.style_image = cv2.imdecode(np.fromstring( engine_fields.style_image.value, dtype=np.uint8 ), cv2.IMREAD_COLOR )
+                    self.style_image = cv2.cvtColor( self.style_image, cv2.COLOR_BGR2RGB)
+                    #print ("got style image")
 
-            consume_frame_style(frame, engine_fields.style)
+            consume_frame_style(frame, engine_fields.style, self.style_image)
 
         self._opencv_adapter = OpencvAdapter(
             preprocess, produce_engine_fields, consume_frame, video_capture,
