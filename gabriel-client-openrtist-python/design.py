c462521ffb902f86ebcb8eae5b0d2c0b19322fbd
Jan Harkes <jaharkes@cs.cmu.edu>
2019-10-31 13:04:20 -0400

    Use specific versions of pyuic5 and protoc
    Avoid subtle version differences in generated code.

--- gabriel-client-openrtist-python/design.py
+++ gabriel-client-openrtist-python/design.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'design.ui'
+# Form implementation generated from reading ui file 'gabriel-client-openrtist-python/design.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.1
 #
