529c2cc4010484723c48c1166660b4c5cd52927f
yutianw <yutianw20@gmail.com>
2023-02-10 17:35:00 -0500

    Changed copyright date to 2023.

--- python-client/sinfonia_wrapper.py
+++ python-client/sinfonia_wrapper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2018 Carnegie Mellon University
+# Copyright 2023 Carnegie Mellon University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
