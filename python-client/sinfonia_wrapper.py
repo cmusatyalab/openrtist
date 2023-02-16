69ba7c05f65cfff9a5beb3e3711cd3735c7243d4
yutianw <yutianw20@gmail.com>
2023-02-16 17:12:44 -0500

    Using parse_known_args and remove parse_args function.

--- python-client/sinfonia_wrapper.py
+++ python-client/sinfonia_wrapper.py
@@ -45,6 +45,13 @@ def launchServer(application_args, use_gpu=False):
 
     logging.info(f"Sending request to sinfonia-tier3 to launch backend.")
 
+    print([
+            sys.executable, 
+            "-m", 
+            "sinfonia_wrapper", # TODO: add relative path to sinfonia_wrapper
+            "-s"
+        ] + application_args)
+
     status = sinfonia_tier3(
         str(tier1_url),
         sinfonia_uuid,
@@ -52,26 +59,26 @@ def launchServer(application_args, use_gpu=False):
             sys.executable, 
             "-m", 
             "sinfonia_wrapper", # TODO: add relative path to sinfonia_wrapper
-            "-s",
-            application_args
-        ]) 
+            "-s"
+        ] + application_args
+        ) 
 
     logging.info(f"Status: {status}")
 
-def parse_args(inputs):
-    """
-    return the bash commands for openrtist as a string
+# def parse_args(inputs):
+#     """
+#     return the bash commands for openrtist as a string
 
-    :param inputs: parse args namespace
-    :return: a string of flags and values after the ui.py command
-    """
+#     :param inputs: parse args namespace
+#     :return: a string of flags and values after the ui.py command
+#     """
 
-    # server_ip = inputs.server_ip
-    v_flag = "-v " + str(inputs.video) if inputs.video else ""
-    d_flag = "-d " + str(inputs.device) if inputs.device else ""
-    fs_flag = "--fullscreen" if inputs.fullscreen else ""
-    all_flags = " ".join([v_flag, d_flag, fs_flag])
-    return all_flags.strip()
+#     # server_ip = inputs.server_ip
+#     v_flag = "-v " + str(inputs.video) if inputs.video else ""
+#     d_flag = "-d " + str(inputs.device) if inputs.device else ""
+#     fs_flag = "--fullscreen" if inputs.fullscreen else ""
+#     all_flags = " ".join([v_flag, d_flag, fs_flag])
+#     return all_flags.strip()
 
 def stage(application_args, timeout=DEFAULT_TIMEOUT):
     timeout = DEFAULT_TIMEOUT # TODO: add timeout as a customized param
@@ -93,13 +100,15 @@ def stage(application_args, timeout=DEFAULT_TIMEOUT):
             raise Exception(f"Connection to backend server timeout after {timeout} seconds.")
 
     try:
+
+        print(application_args)
+
         cmd = [
             sys.executable, 
             "-m", 
             "ui", # TODO: getting UI path
             "openrtist",  # TODO: setting openrtist alias name for customized backend
-            application_args
-        ]
+        ] + application_args
         subprocess.run(cmd)
         logging.info("Frontend terminated.")
         
@@ -111,8 +120,6 @@ def stage(application_args, timeout=DEFAULT_TIMEOUT):
 def main():
     logging.basicConfig(level=logging.INFO)
 
-    print(f"Main method entered! {sys.argv}")
-
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-g", "--gpu", action="store_true", help="Use GPU backend instead of CPU"
@@ -120,36 +127,19 @@ def main():
     parser.add_argument(
         "-s", "--stage", action="store_true", help="Calling the staging for sinfonia, not for external use."
     )
-    # parser.add_argument(
-    #     "server_ip", action="store", help="IP address for Openrtist Server"
-    # )
-    parser.add_argument(
-        "-v", "--video", metavar="URL", type=str, help="video stream (default: try to use USB webcam)"
-    )
-    parser.add_argument(
-        "-d", "--device", type=int, default=-1, help="Capture device (default: -1)"
-    )
-    parser.add_argument(
-        "--fullscreen", action="store_true"
-    )
-    inputs = parser.parse_args()
+
+    inputs, application_args = parser.parse_known_args()
 
     if (inputs.stage):
         logging.warning("Calling internal staging function!")
-        application_args = parse_args(inputs)
         stage(application_args)
     else:
         use_gpu = inputs.gpu
-        application_args = parse_args(inputs)
-
-        print(f"args for program is {application_args}.")
 
         logging.info("Using Sinfonia to open openrtist...")
         launchServer(application_args, use_gpu)
 
     sys.exit(0)
-
-    # sys.exit(app.exec())  # return Dialog Code
 
 
 if __name__ == "__main__":
